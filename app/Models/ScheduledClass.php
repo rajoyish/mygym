457f5f8fa15b8593fd3e42f81ae462e2464e9665
Rajesh Budhathoki <rajoyish@gmail.com>
2023-06-17 21:45:47 +0545

    Create ClassType & ScheduledClass models and migration

---
+++ app/Models/ScheduledClass.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ScheduledClass extends Model
+{
+    use HasFactory;
+
+    public function instructor(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'instructor_id');
+    }
+
+    public function classType(): BelongsTo
+    {
+        return $this->belongsTo(ClassType::class);
+    }
+}
