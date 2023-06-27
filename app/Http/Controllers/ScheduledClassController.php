6d63b35443eba4e186b6a2aaa71f15fde785d242
Rajesh Budhathoki <rajoyish@gmail.com>
2023-06-27 21:51:41 +0545

    Added ScheduledClassPolicy to restrict instructor to delete other's classes

--- app/Http/Controllers/ScheduledClassController.php
+++ app/Http/Controllers/ScheduledClassController.php
@@ -58,7 +58,7 @@ class ScheduledClassController extends Controller
      */
     public function destroy(ScheduledClass $schedule)
     {
-        if (auth()->user()->id !== $schedule->instructor_id) {
+        if (auth()->user()->cannot('delete', $schedule)) {
             abort(403);
         }
 
