818f9cf909ba72468e79a489280ebd4cd79f69f8
Rajesh Budhathoki <rajoyish@gmail.com>
2023-06-27 08:11:08 +0545

    Restrict accessing the navigation with Gate

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @can('schedule-class')
+                    <x-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
+                        Schedule a Class
+                    </x-nav-link>
+                    <x-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                        Upcoming Classes
+                    </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
@@ -70,9 +78,17 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            @can('schedule-class')
+             <x-responsive-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
+                Schedule a Class
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                Upcoming Classes
+            </x-responsive-nav-link>
+            @endcan
         </div>
 
-        <!-- Responsive Settings Options -->
+
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
