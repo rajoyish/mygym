03874dc7aaeb51f9973c7887d1f66ce1540f0881
Rajesh Budhathoki <rajoyish@gmail.com>
2023-06-30 09:32:49 +0545

    Attaching and detaching relationships of member bookings

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\ScheduledClass;
+use Illuminate\Http\Request;
 
 class BookingController extends Controller
 {
@@ -14,5 +15,26 @@ class BookingController extends Controller
             ->get();
 
         return view('member.book')->with('scheduledClasses', $scheduledClasses);
+    }
+
+    public function store(Request $request)
+    {
+        auth()->user()->bookings()->attach($request->scheduled_class_id);
+
+        return redirect()->route('booking.index');
+    }
+
+    public function index()
+    {
+        $bookings = auth()->user()->bookings()->where('date_time', '>', now())->get();
+
+        return view('member.upcoming')->with('bookings', $bookings);
+    }
+
+    public function destroy(int $id)
+    {
+        auth()->user()->bookings()->detach($id);
+
+        return redirect()->route('booking.index');
     }
 }
--- resources/views/member/upcoming.blade.php
+++ resources/views/member/upcoming.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Book a Class
+            Upcoming Classes
         </h2>
     </x-slot>
 
@@ -9,14 +9,12 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="max-w-2xl divide-y p-6 text-gray-900">
-                    @forelse ($scheduledClasses as $class)
+                    @forelse ($bookings  as $class)
                     <div class="py-6">
                       <div class="flex justify-between gap-6">
                         <div>
                           <p class="text-2xl font-bold text-purple-700">{{ $class->classType->name }}</p>
                           <p class="text-sm">{{ $class->instructor->name }}</p>
-                          <p class="mt-2">{{ $class->classType->description }}</p>
-                          <span class="text-sm text-slate-600">{{ $class->classType->minutes }} minutes</span>
                         </div>
                         <div class="flex-shrink-0 text-right">
                           <p class="text-lg font-bold">{{ $class->date_time->format('g:i a') }}</p>
@@ -24,16 +22,16 @@
                         </div>
                       </div>
                       <div class="mt-1 text-right">
-                        <form method="post" action="{{ route('booking.store') }}">
+                        <form method="post" action="{{ route('booking.destroy', $class->id ) }}">
                           @csrf
-                          <input type="hidden" name="scheduled_class_id" value="{{ $class->id }}">
-                          <x-primary-button class="px-3 py-1">Book</x-primary-button>
+                          @method('DELETE')
+                          <x-danger-button class="px-3 py-1" onclick="confirm('Are you sure to cancel this class?')">Cancel</x-danger-button>
                         </form>
                       </div>
                     </div>
                     @empty
                     <div>
-                      <p>No classes are scheduled. Please check back later</p>
+                      <p>You have no upcoming bookings.</p>
                     </div>
                     @endforelse
                   </div>
